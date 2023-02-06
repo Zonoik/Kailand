e1c74f0e4f77b3cf6bd1f04ae5d62baecaddf836
Zonoik <44933715+Zonoik@users.noreply.github.com>
2023-02-06 11:23:13 +0100

    Add files via upload

---
+++ boton.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-btn").addEventListener("click", function() {
+    this.style.backgroundColor = "green";
+    this.style.color = "white";
+    this.style.transform = "scale(1.1)";
+  });
